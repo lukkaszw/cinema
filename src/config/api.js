c4d4e81d9dde81a272022deedb9df8c9d8dbecfd
Lukasz Wojdat <lukaszwojdat88@gmail.com>
2020-05-09 17:35:28 +0200

    Delete apis for current and soon fetching

--- src/config/api.js
+++ src/config/api.js
@@ -2,8 +2,6 @@ const api = {
   url: process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:8000/api',
   endpoints: {
     movies: 'movies',
-    current: 'movies/current',
-    futureMovies: 'movies/soon',
     schedule: 'schedule',
   }
 };
