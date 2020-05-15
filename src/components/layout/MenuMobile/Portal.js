a2df7e674ca614f7cc5c4efca4d036afe37e7d74
Lukasz Wojdat <lukaszwojdat88@gmail.com>
2020-05-15 13:39:00 +0200

    Update portals to be more dynamic

--- src/components/layout/MenuMobile/Portal.js
+++
@@ -1,5 +0,0 @@
-import { createPortal } from 'react-dom';
-
-const Portal = ({ children }) => createPortal(children, document.getElementById('mobile-menu')); 
-
-export default Portal;
--- src/components/layout/Portals/Portals.js
+++ src/components/layout/Portals/Portals.js
@@ -1,7 +1,5 @@
 import { createPortal } from 'react-dom';
 
-export const ModalsPortal = ({ children }) => createPortal(children, document.getElementById('modals'));
+export const ModalsPortal = ({ children, renderPlace }) => createPortal(children, document.getElementById(renderPlace));
 
 export const MenuPortal = ({ children }) => createPortal(children, document.getElementById('mobile-menu')); 
-
-export const AfterMenuPortal =  ({ children }) => createPortal(children, document.getElementById('modals-after-menu')); 
