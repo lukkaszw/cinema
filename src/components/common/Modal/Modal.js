0c9d5c3a55a290023bb9aab5b888524e7cc99d5c
Lukasz Wojdat <lukaszwojdat88@gmail.com>
2020-05-15 13:39:21 +0200

    Use new portal in Modal component

--- src/components/common/Modal/Modal.js
+++ src/components/common/Modal/Modal.js
@@ -2,16 +2,18 @@ import React from 'react';
 import Backdrop from '../Backdrop/Backdrop';
 import CloseBtn from '../CloseBtn/CloseBtn';
 import { ModalsPortal } from '../../layout/Portals/Portals';
+import PropTypes from 'prop-types';
 import styles from './Modal.module.scss';
+import clsx from 'clsx';
 
-const Modal = ({ closeAction, children }) => (
-  <ModalsPortal>
+const Modal = ({ renderPlace, position, closeAction, variant, children}) => (
+  <ModalsPortal renderPlace={renderPlace}>
     <Backdrop 
       isActive={true} 
       closeAction={closeAction}
     />
-    <div className={styles.root}>
-      <div className={styles.content}>
+    <div className={clsx([styles.root, position && styles[position]])}>
+      <div className={clsx([styles.content, variant && styles[variant]])}>
         {children}
       </div>
       <CloseBtn 
@@ -20,5 +22,19 @@ const Modal = ({ closeAction, children }) => (
     </div>
   </ModalsPortal>
 );
+
+Modal.propTyps = {
+  renderPlace: PropTypes.oneOf(['modals', 'modals-after-menu']),
+  closeAction: PropTypes.func.isRequired,
+  variant: PropTypes.oneOf(['dark', 'light']),
+  position: PropTypes.oneOf(['top', 'high', 'center',]),
+  children: PropTypes.node,
+};
+
+Modal.defaultProps = {
+  renderPlace: 'modals',
+  variant: 'light',
+  position: 'center',
+}
  
 export default Modal;
