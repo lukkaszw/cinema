c2747fe73f2e78364def04a66f891242e165a386
Lukasz Wojdat <lukaszwojdat88@gmail.com>
2020-07-13 17:57:08 +0200

    Put OrderMessage in ModalsPortal

--- src/components/features/OrderPanel/OrderMessage/OrderMessage.js
+++ src/components/features/OrderPanel/OrderMessage/OrderMessage.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import Button from '../../../common/Button/Button';
+import Backdrop from '../../../common/Backdrop/Backdrop';
+import { ModalsPortal } from '../../../layout/Portals/Portals';
 import PropTypes from 'prop-types';
 import styles from './OrderMessage.module.scss';
 
 const OrderMessage = ({ message, action, isSuccess }) => {
-  return ( 
-    <div
-      className={styles.root}
-    >
-      <p className={styles.message}>{message}</p>
-      <div className={styles.btn}>
-        <Button
-          action={action}
-          variants={[isSuccess ? 'tertiary' : null]}
-        >
-          Ok
-        </Button>
+  return (
+    <ModalsPortal renderPlace="modals">
+      <Backdrop isActive={true}/>
+      <div
+        className={styles.root}
+      >
+        <p className={styles.message}>{message}</p>
+        <div className={styles.btn}>
+          <Button
+            action={action}
+            variants={[isSuccess ? 'tertiary' : null]}
+          >
+            Ok
+          </Button>
+        </div>
       </div>
-    </div>
+    </ModalsPortal> 
+
    );
 }
 
